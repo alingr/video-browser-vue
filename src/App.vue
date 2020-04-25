033de254d8f7efa408d6a1315869dfd0d6a4b653
alingr <34002917+alingr@users.noreply.github.com>
2020-04-25 22:01:01 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <div class="row">
+      <VideoDetail :video="selectedVideo" />
+      <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
+import VideoDetail from "./components/VideoDetail";
+
+//const API_KEY = "AIzaSyDuRBQFh1nOMfxDInEhzOIpwl_isjOhtHQ"
+//const API_KEY = "AIzaSyAvHZHKBNi3lyJu1yHsZosvJx_cwVYy2Pc"
+const API_KEY = "AIzaSyAaZmhXAztQycnhKDNN5tDMzbR-dsAXBrE"
+
+export default {
+  name: "App",
+  components: {
+    SearchBar,
+    VideoList,
+    VideoDetail
+  },
+  data() {
+    return { videos: [], selectedVideo: null };
+  },
+  methods: {
+    onVideoSelect(video) {
+      this.selectedVideo = video;
+    },
+    onTermChange(searchTerm) {
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: searchTerm
+          }
+        })
+        .then(response => {
+          this.videos = response.data.items;
+        });
+    }
+  }
+};
+</script>
